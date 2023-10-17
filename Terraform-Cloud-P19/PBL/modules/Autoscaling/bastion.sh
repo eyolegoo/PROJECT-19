cef9ed20fb0075f413957d82b9c33d498180e0f5
Godwin <eyolegoo@gmail.com>
2023-10-17 07:47:42 +0100

    new add

--- Terraform-Cloud-P19/PBL/modules/Autoscaling/bastion.sh
+++ Terraform-Cloud-P19/PBL/modules/Autoscaling/bastion.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #--- bastion user data
-yum update -y
-yum install -y mysql 
-yum install -y git tmux 
-yum install -y ansible
+# yum update -y
+# yum install -y mysql 
+# yum install -y git tmux 
+# yum install -y ansible
